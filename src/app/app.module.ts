c76ecc3b572871a210861650bf27faf728af9bbc
David Hohl <david.hohl@gmail.com>
2017-08-13 16:18:05 +0200

    added image picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { ImagePicker } from '@ionic-native/image-picker';
 import {SecureStorage, SecureStorageObject} from '@ionic-native/secure-storage';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -24,6 +25,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SecureStorage,
+    ImagePicker,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {SecureStorage, SecureStorageObject} from '@ionic-native/secure-storage';
+import { ImagePicker } from '@ionic-native/image-picker';
 
 @Component({
   selector: 'page-home',
@@ -8,39 +9,60 @@ import {SecureStorage, SecureStorageObject} from '@ionic-native/secure-storage';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public secureStorage: SecureStorage) {
+  constructor(
+    public navCtrl: NavController,
+    public secureStorage: SecureStorage,
+    private imagePicker: ImagePicker
+  ) {
 
   }
 
+  /**
+   * Secure storage: store key
+   */
   storeKey() {
     this.secureStorage.create('alps-storage').then((storage: SecureStorageObject) => {
       storage.set('password', 'superman')
         .then(
-          data => console.log(data),
+          data => console.log('set secure storage', data),
           error => console.log(error)
         );
     });
   }
 
+  /**
+   * Secure storage: return key
+   */
   getKey() {
     this.secureStorage.create('alps-storage').then((storage: SecureStorageObject) => {
       storage.get('password')
         .then(
-          data => console.log(data),
+          data => console.log('get secure storage', data),
           error => console.log(error)
         );
     });
   }
 
+  /**
+   * Secure storage: remove key
+   */
   removeKey() {
     this.secureStorage.create('alps-storage').then((storage: SecureStorageObject) => {
       storage.remove('password')
         .then(
-          data => console.log(data),
+          data => console.log('remove secure storage', data),
           error => console.log(error)
         );
     });
   }
 
+
+  getImages() {
+    this.imagePicker.getPictures({maximumImagesCount : 2}).then((results) => {
+      for (var i = 0; i < results.length; i++) {
+        console.log('Image URI: ' + results[i]);
+      }
+    }, (err) => { });
+  }
 
 }
